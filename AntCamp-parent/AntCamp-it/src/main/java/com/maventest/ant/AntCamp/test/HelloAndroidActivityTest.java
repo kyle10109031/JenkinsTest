e5f2da207131fc0d39a083388aaedb71af09703d
kyle10109031 <Kyle_Wang@quantatw.com>
2014-03-20 17:53:50 +0800

    first commit

---
+++ AntCamp-parent/AntCamp-it/src/main/java/com/maventest/ant/AntCamp/test/HelloAndroidActivityTest.java
@@ -0,0 +1,39 @@
+package com.maventest.ant.AntCamp.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.maventest.ant.AntCamp.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+	private HelloAndroidActivity mActivity;
+	
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    @Override
+ 	protected void setUp() throws Exception {
+ 		super.setUp();
+
+ 		setActivityInitialTouchMode(false);
+ 		mActivity = getActivity();
+ 	} // end of setUp() method definition
+    
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+    
+    public void testActivity2() {
+    	HelloAndroidActivity activity = getActivity();
+        assertNull(null);
+    }
+    
+    public void testEmma()
+    {
+    	EmmaObject object = new EmmaObject(2, 2);
+//    	assertEquals(4, object.add());
+    }
+}
+
+
---
+++ AntCamp-parent/AntCamp/src/main/java/com/maventest/ant/AntCamp/EmmaObject.java
@@ -0,0 +1,17 @@
+package com.maventest.ant.AntCamp;
+
+public class EmmaObject {
+	int a;
+	int b;
+	
+	public EmmaObject(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int add()
+	{
+		return a+b;
+	}
+}
