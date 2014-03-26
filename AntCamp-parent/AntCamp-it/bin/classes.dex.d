2edea606bb6b3eac43d14e81cf30c6fe8974cdea
kyle10109031 <Kyle_Wang@quantatw.com>
2014-03-26 12:19:32 +0800

    x

--- AntCamp-parent/AntCamp-it/bin/classes.dex.d
+++ AntCamp-parent/AntCamp-it/bin/classes.dex.d
@@ -6,5 +6,4 @@
 /home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R$drawable.class \
 /home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/BuildConfig.class \
 /home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R$string.class \
-/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/R.java.d \
 /home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/dexedLibs/android-junit-report-1.5.8-1ac36f591f04dcde98dfe6ed08042a14.jar \
