122ba07bc3b88758a2444a968a1cbf5984bf41af
kyle10109031 <Kyle_Wang@quantatw.com>
2014-03-26 11:16:06 +0800

    a

---
+++ AntCamp-parent/AntCamp-it/bin/AntCamp-it-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/AntCamp-it-debug-unaligned.apk : \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/AntCamp-it.ap_ \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes.dex \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/libs/android-junit-report-1.5.8.jar \
---
+++ AntCamp-parent/AntCamp-it/bin/classes.dex.d
@@ -0,0 +1,9 @@
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes.dex : \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R$layout.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/HelloAndroidActivityTest.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R$attr.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R$drawable.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/BuildConfig.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/classes/com/maventest/ant/AntCamp/test/R$string.class \
+/home/fl1/Desktop/JenkinsTest/AntCamp-parent/AntCamp-it/bin/dexedLibs/android-junit-report-1.5.8-1ac36f591f04dcde98dfe6ed08042a14.jar \
